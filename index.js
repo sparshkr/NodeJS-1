49a028cf84ba8b745d4ccd10f2b87241471211b7
sparshkr <sparshkumar910@gmail.com>
2024-03-07 19:23:06 +0530

    Env

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get("/", function (req, res) {
     });
   });
 });
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
