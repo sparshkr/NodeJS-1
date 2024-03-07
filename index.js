eadd78aa569c8605674e69a93de828e89c7cfc9e
sparshkr <sparshkumar910@gmail.com>
2024-03-07 20:14:47 +0530

    FInal

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 var express = require("express");
 var app = express();
 
+function getTime(date) {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const monthName = date.toLocaleString("en-US", { month: "long" });
+
+  let ReqIni = date;
+
+  let dayOfMonth = ReqIni.getDate();
+  let formattedDayOfMonth;
+
+  // Handle special cases for 1st, 2nd, and 3rd
+  if (dayOfMonth === 1 || dayOfMonth === 21 || dayOfMonth === 31) {
+    formattedDayOfMonth = dayOfMonth + "st";
+  } else if (dayOfMonth === 2 || dayOfMonth === 22) {
+    formattedDayOfMonth = dayOfMonth + "nd";
+  } else if (dayOfMonth === 3 || dayOfMonth === 23) {
+    formattedDayOfMonth = dayOfMonth + "rd";
+  } else {
+    formattedDayOfMonth = dayOfMonth + "th";
+  }
+
+  let formattedTime = date.toLocaleString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true, // Use 12-hour format
+  });
+
+  let time_requested = `${
+    days[ReqIni.getDay()]
+  } ${formattedDayOfMonth} of ${monthName} ${ReqIni.getFullYear()} ${formattedTime}`;
+  console.log(time_requested);
+
+  return time_requested;
+}
+
 app.get("/", function (req, res) {
   const randomNumber = getRandomNumber(5, 15);
   console.log(randomNumber);
-  let date = new Date();
-  console.log(date);
 
-  let ReqIni = date;
+  let sleep_time = randomNumber;
+  let date = new Date();
+
+  let time_requested = getTime(date);
+
   const p = new Promise(function (resolve) {
     setTimeout(() => {
       date = new Date();
@@ -17,21 +62,14 @@ app.get("/", function (req, res) {
 
   p.then((data) => {
     date = new Date();
-    console.log(data.getHours());
+    let time_processed = getTime(date);
+
     // res.send(data);
-    InitialHour = ReqIni.getHours();
-    InitialMinutes = ReqIni.getMinutes();
-    InitialSeconds = ReqIni.getSeconds();
-    FinalHour = date.getHours();
-    FinalMinutes = date.getMinutes();
-    FinalSeconds = date.getSeconds();
+
     res.json({
-      InitialHour,
-      InitialMinutes,
-      InitialSeconds,
-      FinalHour,
-      FinalMinutes,
-      FinalSeconds,
+      time_requested,
+      sleep_time,
+      time_processed,
     });
   });
 });
